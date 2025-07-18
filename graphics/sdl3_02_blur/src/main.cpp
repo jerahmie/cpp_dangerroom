61738d14230f5876c4218e4100feab995b3017a6
Jerahmie Radder <jerahmie@gmail.com>
2025-07-17 22:11:47 -0500

    Added GLEW, stb_image includes

---
+++ graphics/sdl3_02_blur/src/main.cpp
@@ -0,0 +1,11 @@
+#include <SDL3/SDL.h>
+#include <GL/glew.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int main(){
+    std::cout << "Hello World.\n";
+}
